3ae64361d8eaa2c3879e49f0f7fe54278e580ccf
yaguchi <yaguchi@example.com>
2020-10-03 18:44:57 +0900

    2nd commit

---
+++ sql/createpostcat.sql
@@ -0,0 +1,15 @@
+CREATE TABLE post (
+id integer primary key autoincrement,
+date text,
+catid integer,
+title text,
+text text,
+updated text
+);
+CREATE TABLE cat (
+id integer primary key autoincrement,
+date text,
+title text,
+text text,
+updated text
+);
---
+++ sql/insertpost.sql
@@ -0,0 +1,1 @@
+insert into post (title,text) values ("title2","text2");
